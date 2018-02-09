2c5877769996612a2debe08010146881945a294a
hlere <595806119@qq.com>
2018-02-09 20:02:16 +0800

    删除旧的模版

--- src/store/mutation-types.js
+++
@@ -1,8 +0,0 @@
-/* 重置sotred 的 data 为 出事状态 */
-export const RESET_STORE = 'RESET_STORE'
-
-/* 储存用户的token 和 可查看的产品类型 */
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
-
-export const LOGIN_OUT = 'LOGIN_OUT'
-
--- src/store/mutations.js
+++
@@ -1,9 +0,0 @@
-import * as types from './mutation-types'
-
-export default {
-  [types.RESET_STORE] (state) {
-    state.token = ''
-    state.username = ''
-    state.role = ''
-  }
-}
